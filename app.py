eddbe67158352a7e16dd3fe5f938c1c54b35bff6
emerson <emerson.soares.sc@gmail.com>
2023-04-03 15:56:24 -0300

    commit1

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
 import streamlit as st 
 import pandas as pd
+from main import extrai_dados_preco_popular, extrai_dados_saojoao, extrai_dados_paguemenos, extrai_dados_raia
 
-data = pd.read_csv("rx.xls")
+data = pd.read_excel("rx.xls")
+
+input_ean = str(st.text_input("Digite o EAN do medicamento"))
+if input_ean != "":
+    try:
+        preco_pp = extrai_dados_preco_popular(input_ean)
+    except:
+        preco_pp = "0,00"
+    try:
+        preco_saojoao = extrai_dados_saojoao(input_ean)
+    except:
+        preco_saojoao = "0,00"
+    try:
+        preco_raia = extrai_dados_raia(input_ean)
+    except:
+        preco_raia = "0,00"
+    try:
+        preco_paguemenos = extrai_dados_paguemenos(input_ean)
+    except:
+        preco_paguemenos = "0,00"   
+else:
+    preco_pp = "Digite o EAN do medicamento"
+
+
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.image("image\logo_pp.png", width=100)
+    st.write(f"R$ {str(float(preco_pp[2])).replace('.', ',')}")
+with col2:
+    st.image("image\logo_saojoao.png", width=100)
+    st.write(f"R$ {str(float(preco_saojoao[2])).replace('.', ',')}")
+with col3:
+    st.image("image\logo_paguemenos.png", width=100)
+    st.write(f"R$ {str(float(preco_paguemenos[2])).replace('.', ',')}")
+with col4:
+    st.image("image\logo_raia.png", width=100)
+    st.write(f"R$ {str(float(preco_raia[2])).replace('.', ',')}")
 
 
 
-
-st.write("Hello World")
-st.write(data)
---
+++ main.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+from tqdm import tqdm
+from time import sleep
+
+def extrai_dados_preco_popular(ean):
+    url_cliquefarma = f'https://www.cliquefarma.com.br/preco/site/farmacia-preco-popular/{ean}'
+    headers = {'user-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+    site = requests.get(url_cliquefarma, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    link_interno_cliquefarma = soup.find('a', class_='link-oferta')['href']
+    site_interno_cliquefarma = requests.get(link_interno_cliquefarma, headers=headers)
+    soup_interno_cliquefarma = BeautifulSoup(site_interno_cliquefarma.content, 'html.parser')
+    link = soup_interno_cliquefarma.find('a', class_='inline-block')['href']
+    link_final_preco_popular = link.split('?idsku')[0]
+    site_precopopular = requests.get(link_final_preco_popular, headers=headers)
+    soup_site_precopopular = BeautifulSoup(site_precopopular.content, 'html.parser')
+    cod_preco_popular = soup_site_precopopular.find('div', class_='productReference').get_text()
+    preco_preco_popular = soup_site_precopopular.find('strong', class_='skuBestPrice').get_text()
+    preco_preco_popular = float(preco_preco_popular.split('R$')[1].replace(',', '.'))
+    lst_preco_popular = [link_final_preco_popular, cod_preco_popular, preco_preco_popular]
+    return lst_preco_popular
+
+def extrai_dados_saojoao(ean):
+    url_principal = f'https://www.saojoaofarmacias.com.br/{ean}'
+    headers = {'user-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+    site = requests.get(url_principal, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    link_interno_saojoao = "https://www.saojoaofarmacias.com.br" + f"{soup.find('a', class_='vtex-product-summary-2-x-clearLink vtex-product-summary-2-x-clearLink--vitrine h-100 flex flex-column')['href']}"
+    site_saojoao = requests.get(link_interno_saojoao, headers=headers)
+    soup_site_saojoao = BeautifulSoup(site_saojoao.content, 'html.parser')
+    cod_saojoao = ''
+    preco_saojoao = soup_site_saojoao.find('span', class_='sjdigital-custom-apps-0-x-currencyContainer').get_text()
+    preco_saojoao = float(preco_saojoao.replace('R$','').replace('.','').replace(',','.'))
+    lst_saojoao = [link_interno_saojoao, cod_saojoao, preco_saojoao]
+    return lst_saojoao
+
+def extrai_dados_raia(ean):
+    url_cliquefarma = f'https://www.cliquefarma.com.br/preco/site/drogaraia/{ean}'
+    headers = {'user-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+    site = requests.get(url_cliquefarma, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    link_interno_cliquefarma = soup.find('a', class_='link-oferta')['href']
+    site_interno_cliquefarma = requests.get(link_interno_cliquefarma, headers=headers)
+    soup_interno_cliquefarma = BeautifulSoup(site_interno_cliquefarma.content, 'html.parser')
+    link = soup_interno_cliquefarma.find('a', class_='inline-block')['href']
+    link_final_raia = f"{'https://drogaraia.com.br/' + link.split('.br%2F')[1].split('%3')[0]}"
+    site_raia = requests.get(link_final_raia, headers=headers)
+    soup_site_raia = BeautifulSoup(site_raia.content, 'html.parser')
+    cod_raia = soup_site_raia.find('span', class_='RaiaProductDescriptionstyles__Data-sc-1ijezxr-8').get_text()
+    preco_raia = soup_site_raia.find('span', class_='ProductPricestyles__Price-i0kwh2-4').get_text()
+    preco_raia = float(preco_raia.split('R$ ')[1].replace(',', '.'))
+    lst_raia = [link_final_raia, cod_raia, preco_raia]
+    return lst_raia
+
+def extrai_dados_paguemenos(ean):
+    url_principal = f'https://www.paguemenos.com.br/{ean}'
+    headers = {'user-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+    site = requests.get(url_principal, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    link_interno_paguemenos = "https://www.paguemenos.com.br" + f"{soup.find('a', class_='vtex-product-summary-2-x-clearLink')['href']}"
+    site_paguemenos = requests.get(link_interno_paguemenos, headers=headers)
+    soup_site_paguemenos = BeautifulSoup(site_paguemenos.content, 'html.parser')
+    cod_paguemenos = soup_site_paguemenos.find('div', class_='pr0')
+    cod_paguemenos = cod_paguemenos.find('p').get_text()
+    cod_paguemenos = cod_paguemenos.split(' ')[1]
+    preco_paguemenos = soup_site_paguemenos.find('span', class_='vtex-store-components-3-x-sellingPrice vtex-store-components-3-x-sellingPriceValue t-heading-2-s dib ph2 vtex-store-components-3-x-price_sellingPrice').get_text()
+    preco_paguemenos = float(preco_paguemenos.replace('R$','').replace('.','').replace(',','.'))
+    lst_paguemenos = [link_interno_paguemenos, cod_paguemenos, preco_paguemenos]
+    return lst_paguemenos
